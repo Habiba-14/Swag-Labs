f0bf044968bbb50d95318b982f97bde468841af4
habiba <habeba.naser1412@gmail.com>
2024-10-15 19:06:22 +0300

    Login without password test

--- src/test/java/EmptyPass.java
+++ src/test/java/EmptyPass.java
@@ -1,2 +1,13 @@
-public class EmptyPass {
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+public class EmptyPass extends Start{
+
+    @Test
+    public void SubmitEmptyPassword(){
+        driver.findElement(By.id("user-name")).sendKeys("locked_out_user");
+        driver.findElement(By.id("login-button")).click();
+        String mssg = driver.findElement(By.xpath("//*[@id=\"login_button_container\"]/div/form/div[3]/h3")).getText();
+        System.out.println(mssg);
+    }
 }
--- src/test/java/InvalidPassword.java
+++ src/test/java/InvalidPassword.java
@@ -1,2 +1,19 @@
-public class InvalidPassword {
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+public class InvalidPassword extends Start {
+    String[] acceptedUsernames = {"standard_user", "locked_out_user", "problem_user", "performance_glitch_user", "error_user","visual_user"};
+
+    @Test
+    public void InvalidPasswordLogin(){
+        for(int i =0 ; i< acceptedUsernames.length ; i++) {
+            driver.findElement(By.id("user-name")).sendKeys(acceptedUsernames[i]);
+            driver.findElement(By.id("password")).sendKeys("pass");
+            driver.findElement(By.id("login-button")).click();
+            String errorMssg = driver.findElement(By.xpath("//*[@id=\"login_button_container\"]/div/form/div[3]/h3")).getText();
+            System.out.println(errorMssg);
+            driver.navigate().to("https://www.saucedemo.com/");
+        }
+
+    }
 }
