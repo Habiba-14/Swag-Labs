8c8acb58fc7a0e9a4d4b597dd1b1171cb2d602fe
habiba <habeba.naser1412@gmail.com>
2024-10-15 19:06:44 +0300

    Login with invalid username test

--- src/test/java/InvalidUserName.java
+++ src/test/java/InvalidUserName.java
@@ -1,2 +1,13 @@
-public class InvalidUserName {
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+public class InvalidUserName extends Start{
+    @Test
+    public void SubmitInvalidUserName(){
+        driver.findElement(By.id("user-name")).sendKeys("Habiba1412");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.id("login-button")).click();
+        String mssg = driver.findElement(By.xpath("//*[@id=\"login_button_container\"]/div/form/div[3]/h3")).getText();
+        System.out.println(mssg);
+    }
 }
