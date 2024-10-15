148651b62d8a575e9c019b4047cc0113c9618359
habiba <habeba.naser1412@gmail.com>
2024-10-15 19:06:01 +0300

    Login without email test

--- src/test/java/EmptyEmail.java
+++ src/test/java/EmptyEmail.java
@@ -1,2 +1,13 @@
-public class EmptyEmail {
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+public class EmptyEmail extends Start{
+
+    @Test
+    public void SubmitEmptyEmail(){
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.id("login-button")).click();
+        String mssg = driver.findElement(By.xpath("//*[@id=\"login_button_container\"]/div/form/div[3]/h3")).getText();
+        System.out.println(mssg);
+    }
 }
