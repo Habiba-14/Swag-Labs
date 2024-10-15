6448e41de2191ab3f94ca4312407c887f67d1f5b
habiba <habeba.naser1412@gmail.com>
2024-10-15 18:47:34 +0300

    Valid Login

--- src/test/java/ValidLogin.java
+++ src/test/java/ValidLogin.java
@@ -1,2 +1,17 @@
-public class ValidLogin {
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+public class ValidLogin extends Start{
+    // Declare and initialize an array in one step
+    String[] acceptedUsernames = {"standard_user", "locked_out_user", "problem_user", "performance_glitch_user", "error_user","visual_user"};
+
+    @Test
+    public void ValidLogin(){
+        for(int i =0 ; i< acceptedUsernames.length ; i++) {
+            driver.findElement(By.id("user-name")).sendKeys(acceptedUsernames[i]);
+            driver.findElement(By.id("password")).sendKeys("secret_sauce");
+            driver.findElement(By.id("login-button")).click();
+            driver.navigate().to("https://www.saucedemo.com/");
+        }
+    }
 }
